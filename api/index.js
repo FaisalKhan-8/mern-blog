4622f341ae5d80a8fa11a23dfbaebf75cb740c28
FaisalKhan-8 <mdfaisalkhan1500@gmail.com>
2024-01-18 15:35:02 +0530

    connect to the database

--- api/index.js
+++ api/index.js
@@ -1,6 +1,19 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
+dotenv.config();
 const app = express();
+
+const db = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log('Connected to MongoDB');
+  } catch (error) {
+    console.log(error);
+  }
+};
+db();
 
 const port = 3000;
 app.listen(port, () => {
