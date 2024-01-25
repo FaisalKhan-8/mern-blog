5b51227be53e1eb1f4d17d86bd66f43d04f9655c
FaisalKhan-8 <mdfaisalkhan1500@gmail.com>
2024-01-25 10:19:22 +0530

    some changes in profile page.

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -7,37 +7,39 @@ const DashProfile = () => {
     <>
       <div className='profile_container'>
         <h1>Profile</h1>
-        <form className='form-container'>
-          <div className='profile-img'>
-            <img src={currentUser.profilePicture} alt='user' />
-          </div>
-          <div className='profile_input'>
-            <input
-              type='text'
-              id='username'
-              placeholder='username'
-              defaultValue={currentUser.username}
-            />
-          </div>
-          <div className='profile_input'>
-            <input
-              type='email'
-              id='email'
-              placeholder='Email'
-              defaultValue={currentUser.email}
-            />
-          </div>
-          <div className='profile_input'>
-            <input type='password' id='password' placeholder='Password' />
-          </div>
+        <div>
+          <form className='form-container'>
+            <div className='profile-img'>
+              <img src={currentUser.profilePicture} alt='user' />
+            </div>
+            <div className='profile_input'>
+              <input
+                type='text'
+                id='username'
+                placeholder='username'
+                defaultValue={currentUser.username}
+              />
+            </div>
+            <div className='profile_input'>
+              <input
+                type='email'
+                id='email'
+                placeholder='Email'
+                defaultValue={currentUser.email}
+              />
+            </div>
+            <div className='profile_input'>
+              <input type='password' id='password' placeholder='Password' />
+            </div>
 
-          <button type='submit' className='profile_update_btn'>
-            Update
-          </button>
-        </form>
-        <div className='profile-d'>
-          <span className='profile_delete_btn'>Delete Account</span>
-          <span className='profile_delete_btn'>Sign Out</span>
+            <button type='submit' className='profile_update_btn'>
+              Update
+            </button>
+          </form>
+          <div className='profile-d'>
+            <span className='profile_delete_btn'>Delete Account</span>
+            <span className='profile_delete_btn'>Sign Out</span>
+          </div>
         </div>
       </div>
     </>
