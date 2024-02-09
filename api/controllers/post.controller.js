67831bd54c2d93798e0d72e316582c78c8837595
FaisalKhan-8 <mdfaisalkhan1500@gmail.com>
2024-02-09 13:58:56 +0530

    create get posts api routes

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -30,3 +30,50 @@ export const createPost = async (req, res, next) => {
     next(error);
   }
 };
+
+export const getPosts = async (req, res, next) => {
+  try {
+    const startIndex = parseInt(req.query.startIndex) || 0;
+    const limit = parseInt(req.query.limit) || 9;
+    const sortDirection = req.query.order === 'asc' ? 1 : -1;
+    const posts = await Post.find({
+      ...(req.query.userId && { userId: req.userId }),
+      ...(req.query.category && { category: req.category }),
+      ...(req.query.postId && { _id: req.postId }),
+      ...(req.query.searchTerm && {
+        $or: [
+          //: i means is a search term is lowercase or not does not matter.
+          { title: { $regex: req.searchTerm, $options: 'i' } },
+          { content: { $regex: req.searchTerm, $options: 'i' } },
+        ],
+      }),
+    })
+      .sort({ updatedAt: sortDirection })
+      .skip(startIndex)
+      .limit(limit);
+
+    const totalPosts = await Post.countDocuments();
+
+    // a month post should have created
+    const now = new Date();
+
+    const oneMonthAgo = new Date(
+      now.getFullYear(),
+      now.getMonth() - 1,
+      now.getDate()
+    );
+
+    const lastMonthPosts = await Post.countDocuments({
+      createdAt: { $gte: oneMonthAgo },
+    });
+
+    res.status(200).json({
+      posts,
+      totalPosts,
+      lastMonthPosts,
+    });
+    console.log(posts);
+  } catch (error) {
+    next(error);
+  }
+};
--- api/routes/post.routes.js
+++ api/routes/post.routes.js
@@ -1,9 +1,9 @@
 import express from 'express';
 import { VerifyUser } from '../utils/VerifyUser.js';
-import { createPost } from '../controllers/post.controller.js';
+import { createPost, getPosts } from '../controllers/post.controller.js';
 
 const router = express.Router();
 
-router.post('/create', VerifyUser, createPost);
+router.post('/create', VerifyUser, createPost).get('/getposts', getPosts);
 
 export default router;
