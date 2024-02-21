760db54368a877f8462850a084bdc6f02d3d6727
FaisalKhan-8 <mdfaisalkhan1500@gmail.com>
2024-02-21 15:45:49 +0530

    complete about and projects pages

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,7 +1,58 @@
 import React from 'react';
 
 const About = () => {
-  return <div>About</div>;
+  const githubUrl = 'https://github.com/FaisalKhan-8';
+  const linkedinUrl = 'https://www.linkedin.com/in/faisal-khan-ab4524184/';
+
+  return (
+    <div className='about-page-main'>
+      <div className='about-page'>
+        <div className='about-head'>
+          <h1>About BeatBlog</h1>
+          <div className='about-page-text'>
+            <p>
+              Welcome to BeatBlog, This blog was created by Faisal Khan. as a
+              personal project to share his thoughts and ideas with the world.
+              BeatBlog aims to provide valuable insights, tutorials, and
+              resources related to web development, programming, and other
+              technology topics.
+            </p>
+
+            <p>
+              On this blog, you'll find weekly articles and tutorials on topics
+              such as web development, software engineering, and programming
+              languages. Faisal is always learning and exploring new
+              technologies, so be sure to check back often for new content!
+            </p>
+
+            <p>
+              We encourage you to leave comments on our posts and engage with
+              other readers. You can like other people's comments and reply to
+              them as well. We believe that a community of learners can help
+              each other grow and improve.
+            </p>
+
+            <p>Check out my social</p>
+
+            <div className='social-links'>
+              <a
+                href={githubUrl}
+                target='_blank'
+                rel='noopener noreferrer'>
+                GitHub
+              </a>
+              <a
+                href={linkedinUrl}
+                target='_blank'
+                rel='noopener noreferrer'>
+                LinkedIn
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default About;
--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import CallToAction from '../components/CallToAction';
 
 const Projects = () => {
-  return <div>Projects</div>;
+  return (
+    <div className='Projects_container'>
+      <h1>Projects</h1>
+      <p>
+        Build fun and engaging projects while learning HTML, CSS, and
+        JavaScript!
+      </p>
+      <CallToAction />
+    </div>
+  );
 };
 
 export default Projects;
