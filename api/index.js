b3cd09ac0720b8887d5bd8d46a5e91f21961d7c6
FaisalKhan-8 <mdfaisalkhan1500@gmail.com>
2024-02-22 11:13:12 +0530

    some changes

--- api/index.js
+++ api/index.js
@@ -7,19 +7,20 @@ import path from 'path';
 
 dotenv.config();
 
+// database Call here....
+db();
+
+const __dirname = path.resolve();
+
 // middleware....
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
-const __dirname = path.resolve();
-
-// database Call here....
-db();
 // server configuration....
-app.listen(process.env.PORT || 3000, () => {
-  console.log(`Server running on port ${process.env.PORT}`);
+app.listen(3000, () => {
+  console.log('Server is running on port 3000!');
 });
 
 // Routes import here....
@@ -35,6 +36,7 @@ app.use('/api/post', postRoutes);
 app.use('/api/comment', commentRoutes);
 
 app.use(express.static(path.join(__dirname, '/client/dist')));
+
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
 });
